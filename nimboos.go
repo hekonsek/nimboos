17b2d227af1ce4eba0c18ff62bd501e31331b11d
Henryk Konsek <hekonsek@gmail.com>
2018-02-28 10:19:03 +0100

    Added env-create Ansible playbook.

--- nimboos.go
+++ nimboos.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"os"
+)
 
 func main() {
-	data, err := Asset("data/hello.txt")
+	envCreateYml, err := Asset("data/env-create.yml")
+	exitOnError(err)
+
+	err = ioutil.WriteFile("/tmp/nimboos-env-create.yml", envCreateYml, 0644)
+	exitOnError(err)
+
+	cmd := exec.Command("ansible-playbook", "/tmp/nimboos-env-create.yml", "-e vpc_name=nimboos_test1")
+	stdoutStderr, err := cmd.CombinedOutput()
+	exitOnError(err)
+	fmt.Println(string(stdoutStderr))
+}
+
+func exitOnError(err error) {
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(-1)
 	}
-	fmt.Println(string(data))
 }
